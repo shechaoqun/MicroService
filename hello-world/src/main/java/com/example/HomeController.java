932bc9ce7cf9ddaad6770e1af08c9c39b5957cee
shechaoqun@gmail.com <shechaoqun@gmail.com>
2017-04-27 16:08:48 +0800

    test

---
+++ hello-world/src/main/java/com/example/HomeController.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by chaoqunshe on 4/21/17.
+ */
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "hello world docer boot ";
+    }
+}
